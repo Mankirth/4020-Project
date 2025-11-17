0bc843f546682ceda420ce5fb59379386d57f0c7
Mankirth <159180334+Mankirth@users.noreply.github.com>
2025-11-17 00:56:26 -0500

    merge fix

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 import Chart from 'chart.js/auto';
-const mongoose = require('mongoose');
-const http = require('http');
+import mongoose from 'mongoose';
+import http from 'http';
 const PORT = 3000;
-let WebSocketServer = require('ws').Server;
+import { WebSocketServer } from 'ws';
 let wss = new WebSocketServer({port: 80});
 
 wss.on('connection', function(ws){
@@ -15,7 +15,6 @@ wss.on('connection', function(ws){
 console.log('initializing WebSocket');
 
 main().catch(err => console.log(err));
-message = "Heloo";
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
@@ -38,56 +37,56 @@ async function main() {
     var compQuestions = await Question.find({domain: 'computer_security'}).limit(50);
     var historyQuestions = await Question.find({domain: 'prehistory'}).limit(50);
     var sociologyQuestions = await Question.find({domain: 'sociology'}).limit(50);
-    compQuestions = askGpt(compQuestions);
-    historyQuestions = askGpt(historyQuestions);
-    sociologyQuestions = askGpt(sociologyQuestions);
+    var compQuestions = askGpt(compQuestions);
+    var historyQuestions = askGpt(historyQuestions);
+    var sociologyQuestions = askGpt(sociologyQuestions);
     makeChart(compQuestions, historyQuestions, sociologyQuestions);
 }
 
 function makeChart(compQuestions, historyQuestions, sociologyQuestions){
-    compScore, compAvg = evaluate(compQuestions);
-    historyScore, historyAvg = evaluate(historyQuestions);
-    sociologyScore, sociologyAvg = evaluate(sociologyQuestions);
-    ctx = document.getElementById('myChart');
+    // var compScore, compAvg = evaluate(compQuestions);
+    // var historyScore, historyAvg = evaluate(historyQuestions);
+    // var sociologyScore, sociologyAvg = evaluate(sociologyQuestions);
+    // var ctx = document.getElementById('myChart');
 
-    compData = [];
-    histData = [];
-    soscData = [];
+    // var compData = [];
+    // var histData = [];
+    // var soscData = [];
 
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-        labels: ['Computer Science', 'History', 'Social Science'],
-        datasets: [{
-            label: 'Accuracy',
-            data: [compScore, historyScore, sociologyScore],
-            borderWidth: 1
-        }]
-        },
-        options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
-        }
-    });
+    // new Chart(ctx, {
+    //     type: 'bar',
+    //     data: {
+    //     labels: ['Computer Science', 'History', 'Social Science'],
+    //     datasets: [{
+    //         label: 'Accuracy',
+    //         data: [compScore, historyScore, sociologyScore],
+    //         borderWidth: 1
+    //     }]
+    //     },
+    //     options: {
+    //     scales: {
+    //         y: {
+    //         beginAtZero: true
+    //         }
+    //     }
+    //     }
+    // });
 }
 
 function askGpt(questions){
-    for(i = 0; i < 50; i++){
-        currentQ = questions[i];
-        gptPrompt = currentQ.question, " A.", currentQ.A, " B. ", currentQ.B, " C. ", currentQ.C, " D. ", currentQ.D;
+    for(let i = 0; i < 50; i++){
+        let currentQ = questions[i];
+        let gptPrompt = currentQ.question + " A." + currentQ.A + " B. " + currentQ.B + " C. " + currentQ.C + " D. " + currentQ.D;
         //ask chatGPT with prompt, store response and response time
     }
     return questions;
 }
 
 function evaluate(questions){
-    score = 0
-    avg = 0;
-    for(i = 0; i < 50; i++){
-        currentQ = questions[i];
+    let score = 0
+    let avg = 0;
+    for(let i = 0; i < 50; i++){
+        let currentQ = questions[i];
         if(currentQ.expected_response == currentQ.chatgpt_response)
             score++;
         avg += currentQ.response_time;
